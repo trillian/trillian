1afe2d901f99f48b40cad25c496811a7077803e9
Demitri Muna <demitri.muna@gmail.com>
2016-08-20 11:12:53 -0400

    Adding PGGeometry database file - add support for PostgreSQL geometry data types with SQLAlchemy.

---
+++ python/trillian/database/PGGeometry.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+
+__author__ = "Demitri Muna"
+
+'''
+Classes to add support for PostrgeSQL geometric data tpyes that SQLAlchemy doesn't natively support.
+
+Example usage, e.g. at the top of a ModelClasses file:
+
+from sqlalchemy.dialects.postgresql import base as pg
+from .PGGeometry import PGPoint
+pg.ischema_names['point'] = PGPoint
+
+This will assign the PGPoint object type for all fields of type 'point'.
+
+'''
+
+import sqlalchemy.types as types
+
+class PGPoint(types.UserDefinedType):
+	'''
+	Class to represent the PostgreSQL "point" datatype.
+	
+	Ref: https://www.postgresql.org/docs/9.5/static/datatype-geometric.html
+	
+	For illustrative purposes in the comments below, assume a column defined as:
+	CREATE TABLE some_table (
+		pt point
+	);
+	
+	Using this datatype definition, tuples of float values can be provided
+	to point columns, and tuples of float values will be returned.
+	
+	'''
+	def bind_processor(self, dialect):
+		'''
+		Return a function that performs the conversion from the
+		provided object to a form that PostgreSQL can understand.
+		
+		To insert a value into a 'point' field:
+			INSERT INTO some_table (pt) VALUES ('1,2');
+		'''
+		def process(value):
+			if value is None:
+				return value
+			return "'{0},{1}'".format(value[0], value[1])
+		return process
+	
+	def result_processor(self, dialoect, coltype):
+		'''
+		Return a function that converts the value that comes from the
+		database to a Python object.
+		'''
+		def process(value):
+			if value is None:
+				return None
+			# value will be a string of the form (without the quotes): '1,2'
+			point_values = value.split(",") # not sure if there will be surrounding quotes
+			return (float(point_values[0]), float(point_values[1]))
+		return process
+
